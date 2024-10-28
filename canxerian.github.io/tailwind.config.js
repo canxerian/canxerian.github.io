55365823f2980c01ddeb31767332c993150144ad
Mark Nguyen <1138440+canxerian@users.noreply.github.com>
2024-10-28 12:45:37 +0000

    Remove Vita boilerplate; start using tailwind

--- canxerian.github.io/tailwind.config.js
+++ canxerian.github.io/tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["*.{html,js}"],
   theme: {
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+    },
     extend: {},
+
   },
   plugins: [],
 }
