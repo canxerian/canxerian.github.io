915a9437e6d28425e733d0169f49dbd40074a96d
Mark Nguyen <1138440+canxerian@users.noreply.github.com>
2024-10-28 15:00:59 +0000

    Setup Lenis scroll

--- canxerian.github.io/src/main.ts
+++ canxerian.github.io/src/main.ts
@@ -1,1 +1,17 @@
-import "./main.css"
+import Lenis from 'lenis'
+import 'lenis/dist/lenis.css'
+
+// Initialize Lenis
+const lenis = new Lenis({});
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+});
+
+// Use requestAnimationFrame to continuously update the scroll
+function raf(time: number) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
