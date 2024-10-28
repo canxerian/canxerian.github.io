3b1e7d118572c9b96e9b7173712ced47322e418c
Mark Nguyen <1138440+canxerian@users.noreply.github.com>
2024-10-28 16:00:31 +0000

    Multipage setup

--- canxerian.github.io/vite.config.js
+++ canxerian.github.io/vite.config.js
@@ -1,10 +1,19 @@
-import { defineConfig } from "vite";
+import { resolve } from 'path';
 import tailwindcss from "tailwindcss";
+import { defineConfig } from "vite";
 
 export default defineConfig({
     css: {
         postcss: {
             plugins: [tailwindcss()],
+        },
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                nested: resolve(__dirname, 'nested/index.html'),
+            },
         },
     },
 });
