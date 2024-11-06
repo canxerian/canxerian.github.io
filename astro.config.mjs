92a95e4e3de731ec5be92bd54f8a012b2a56c2e1
Mark Nguyen <1138440+canxerian@users.noreply.github.com>
2024-11-06 23:10:28 +0000

    unpic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import { imageService } from "@unpic/astro/service";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://canxerian.com',
+  image: {
+    service: imageService({
+      placeholder: "blurhash",
+    }),
+  }
 });
