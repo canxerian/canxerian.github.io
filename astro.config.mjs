7f8d1be283b2d9b021e8fe03fd3bf4e5d3f9a6ba
Mark Nguyen <1138440+canxerian@users.noreply.github.com>
2024-11-08 12:56:33 +0000

    More lusion content

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,12 @@ import { imageService } from "@unpic/astro/service";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://canxerian.com',
+  site: 'https://mark-n.co',
+  markdown: {
+    shikiConfig: {
+      theme: 'nord',
+    },
+  },
   image: {
     service: imageService({
       placeholder: "blurhash",
